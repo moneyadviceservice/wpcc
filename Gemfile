ba3df4a5887efbd137c05b1151dac411e95a0d9b
John Player <johnplayer1982@msn.com>
2017-06-16 14:33:12 +0100

    Tries to add the JS for dough

--- Gemfile
+++ Gemfile
@@ -6,14 +6,17 @@ source 'http://gems.dev.mas.local'
 # development dependencies will be added by default to the :development group.
 gemspec
 
+ruby '2.2.5'
+
 gem 'rails', '~> 4.2.7'
 gem 'bowndler', git: 'https://github.com/moneyadviceservice/bowndler'
 gem 'sass-rails'
+gem 'jquery-rails'
+gem 'turbolinks'
 
 group :development, :test do
   gem 'pry-rails'
   gem 'rspec-rails', '~> 3.5'
-
 end
 
 group :build, :test do
--- spec/dummy/config/initializers/assets.rb
+++ spec/dummy/config/initializers/assets.rb
@@ -1,8 +1,12 @@
-Rails.application.configure do
-  
-  config.assets.paths << Rails.root.join('..', '..', 'vendor', 'assets', 'bower_components')
+# Add additional assets to the asset load path
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
 
-  # Application JavaScript
-  config.assets.precompile += %w(dough/assets/js/lib/*.js
-                                 dough/assets/js/components/*.js)
-end
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+Rails.application.config.assets.precompile += %w(
+  application.css
+  dough/assets/js/components/DoughBaseComponent.js
+  dough/assets/js/lib/componentLoader.js
+  jquery/dist/jquery.js
+  requirejs/require.js
+  rsvp/rsvp.js)
