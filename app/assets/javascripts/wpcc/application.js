5a207b370509fef83b667aee76b6bdc285ffbab0
John Player <johnplayer1982@msn.com>
2017-06-19 14:31:42 +0100

    Adds templates to dummy application layout

--- app/assets/javascripts/wpcc/application.js
+++ app/assets/javascripts/wpcc/application.js
@@ -1,11 +1,3 @@
 //= require jquery
 //= require jquery_ujs
-//= require require_config
 //= require_self
-
-// Components
-require(['jquery', 'componentLoader'], function ($, componentLoader) {
-  'use strict';
-
-  componentLoader.init($('body'));
-});
--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  layout 'application'
 
   before_action :set_locale
+
+  def parent_template
+    'layouts/application'
+  end
+  helper_method :parent_template
 
   private
 
