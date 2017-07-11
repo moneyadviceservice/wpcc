333ec2d1c6a0179a972ba89652ffa3d9989a0d09
Will Hall <willhall88@gmail.com>
2017-07-11 11:13:38 +0100

    Fix namespacing of wpcc module

--- app/models/wpcc/minimum_contribution_calculator.rb
+++ app/models/wpcc/minimum_contribution_calculator.rb
@@ -1,28 +1,26 @@
 require 'active_model'
 
-module Wpcc
-  class MinimumContributionCalculator < ContributionCalculator
-    def eligible_salary
-      return upper_less_lower_limit if salary > upper_earnings_threshold
-      return salary_less_lower_limit if salary <= upper_earnings_threshold
-    end
+class Wpcc::MinimumContributionCalculator < Wpcc::ContributionCalculator
+  def eligible_salary
+    return upper_less_lower_limit if salary > upper_earnings_threshold
+    return salary_less_lower_limit if salary <= upper_earnings_threshold
+  end
 
-    def employee_percent
-      1
-    end
+  def employee_percent
+    1
+  end
 
-    def employer_percent
-      1
-    end
+  def employer_percent
+    1
+  end
 
-    private
+  private
 
-    def upper_less_lower_limit
-      upper_earnings_threshold - lower_earnings_threshold
-    end
+  def upper_less_lower_limit
+    upper_earnings_threshold - lower_earnings_threshold
+  end
 
-    def salary_less_lower_limit
-      salary - lower_earnings_threshold
-    end
+  def salary_less_lower_limit
+    salary - lower_earnings_threshold
   end
 end
