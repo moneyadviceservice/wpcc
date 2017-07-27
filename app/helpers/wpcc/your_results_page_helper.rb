50d160ecca483ba728243e79ed345c6bb1296492
Jones Agyemang <mightyj@hotmail.co.uk>
2017-07-27 15:19:27 +0100

    fixup! Adds tax relief warning for low earners

--- app/helpers/wpcc/your_results_page_helper.rb
+++ app/helpers/wpcc/your_results_page_helper.rb
@@ -1,9 +1,14 @@
 module Wpcc
   module YourResultsPageHelper
-    TAX_RELIEF_THRESHOLD_RATE = 11_500
+    TAX_RELIEF_THRESHOLD_RATE = {
+      year: 11_500,
+      month: 958.33,
+      fourweeks: 884.61,
+      week: 221.15
+    }
 
-    def earning_below_tax_relief_threshold?(salary)
-      salary.to_i < TAX_RELIEF_THRESHOLD_RATE
+    def earning_below_tax_relief_threshold?(salary, salary_frequency)
+      salary.to_f < TAX_RELIEF_THRESHOLD_RATE[salary_frequency.to_sym]
     end
   end
 end
