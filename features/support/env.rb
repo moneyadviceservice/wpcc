4a0c80ed7dc44ea077016a527ce87fe7abc5f71b
Tomas D'Stefano <tomas_stefano@successoft.com>
2017-08-07 19:47:24 +0100

    Add js scenarios when change frequencies on last step
    * Added poltergeist gem to the project to be possible
    to test the javascript via cucumber with @javascript tag
    enabled.

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,5 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 ENV['RAILS_ROOT'] ||= File.dirname(__FILE__) + '../../../spec/dummy'
 
 require 'cucumber/rails'
+require 'capybara/poltergeist'
 
 ActionController::Base.allow_rescue = false
+Capybara.javascript_driver = :poltergeist
