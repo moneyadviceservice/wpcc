dd751504e6165d9c2a6061c0e41c508af38bf577
Tomas D'Stefano <tomas_stefano@successoft.com>
2017-08-07 19:59:33 +0100

    Remove contribution_data methods from presenter and renaming accordingly
    The js is using the values (without the number formatting) so I renamed
    those method with the "formatted" prefix in order to don't have 8 methods
    just to get contributions formatted or not formatted

--- app/presenters/wpcc/period_contribution_presenter.rb
+++ app/presenters/wpcc/period_contribution_presenter.rb
@@ -4,31 +4,19 @@ module Wpcc
       t("wpcc.results.period_title.#{name}")
     end
 
-    def employee_contribution
+    def formatted_employee_contribution
       formatted_contribution(object.employee_contribution)
     end
 
-    def employee_contribution_data
-      object.employee_contribution
-    end
-
-    def employer_contribution
+    def formatted_employer_contribution
       formatted_contribution(object.employer_contribution)
     end
 
-    def employer_contribution_data
-      object.employer_contribution
-    end
-
-    def tax_relief
+    def formatted_tax_relief
       "(includes tax relief of #{formatted_contribution(object.tax_relief)})"
     end
 
-    def tax_relief_data
-      object.tax_relief
-    end
-
-    def total_contributions
+    def formatted_total_contributions
       formatted_contribution(object.total_contributions)
     end
 
