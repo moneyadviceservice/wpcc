7ceb886b871644a67a4b5924961dad82b3885a2c
David Trussler <mail@davidtrussler.net>
2017-08-22 16:05:28 +0100

    TP: 8213, Comment: Add testable values to Karma test

--- spec/javascripts/tests/Email_spec.js
+++ spec/javascripts/tests/Email_spec.js
@@ -28,6 +28,27 @@ describe.only('Email', function() {
 
       console.log(this.component);
 
+      var message =
+        '1. Your details: 31 years, male, £50,000 per year, part salary' +
+        '2. Your contributions: You: 1%, Your employer: 1%' +
+        '3. Results:' +
+        'Qualifying Earnings: £39,124' +
+        'Now:' +
+        'Your contribution: £32.60' +
+        '(includes tax relief of £6.52)' +
+        'Employer\'s contribution: £32.60' +
+        'Total contributions: £65.20' +
+        'April 2018 - March 2019:' +
+        'Your contribution: £97.81' +
+        '(includes tax relief of £19.56)' +
+        'Employer\'s contribution: £65.21' +
+        'Total contributions: £163.02' +
+        'April 2019 onwards:' +
+        'Your contribution: £163.02' +
+        '(includes tax relief of £32.60)' +
+        'Employer\'s contribution: £97.81' +
+        'Total contributions: £260.83';
+
       expect(1).to.equal(1);
     });
   });
