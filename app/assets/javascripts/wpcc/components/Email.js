b70756a1fab8a79674e8393d876f707b2a507bb7
David Trussler <mail@davidtrussler.net>
2017-08-23 09:55:51 +0100

    TP: 8213, Comment: Creates mock href string for email link and tests it

--- app/assets/javascripts/wpcc/components/Email.js
+++ app/assets/javascripts/wpcc/components/Email.js
@@ -6,6 +6,8 @@ define(['jquery', 'DoughBaseComponent'], function($, DoughBaseComponent) {
 
   Email = function($el, config) {
     Email.baseConstructor.call(this, $el, config, defaultConfig);
+
+    this.$emailLink = this.$el.find('[data-dough-email-link]');
   };
 
   /**
@@ -17,7 +19,39 @@ define(['jquery', 'DoughBaseComponent'], function($, DoughBaseComponent) {
 
   Email.prototype.init = function(initialised) {
     this._initialisedSuccess(initialised);
+    this._setUpLink();
   };
+
+  Email.prototype._setUpLink = function() {
+    var message = this._getMessage();
+    var href = 'mailto:?body=' + message;
+
+    this.$emailLink.prop('href', href);
+  }
+
+  Email.prototype._getMessage = function() {
+    var message = '';
+
+    message += '1. Your details: 31 years, male, £50,000 per year, part salary\n\n';
+    message += '2. Your contributions: You: 1%, Your employer: 1%\n\n';
+    message += '3. Results: Qualifying Earnings: £39,124\n\n';
+    message += 'Now\n';
+    message += 'Your contribution: £32.60 (includes tax relief of £6.52)\n';
+    message += 'Employer\'s contribution: £32.60\n';
+    message += 'Total contributions: £65.20\n\n';
+    message += 'April 2018 - March 2019:\n';
+    message += 'Your contribution: £97.81\n';
+    message += '(includes tax relief of £19.56)\n';
+    message += 'Employer\'s contribution: £65.21\n';
+    message += 'Total contributions: £163.02\n\n';
+    message += 'April 2019 onwards:\n';
+    message += 'Your contribution: £163.02\n';
+    message += '(includes tax relief of £32.60)\n';
+    message += 'Employer\'s contribution: £97.81\n';
+    message += 'Total contributions: £260.83';
+
+    return encodeURIComponent(message);
+  }
 
   return Email;
 });
--- spec/javascripts/tests/Email_spec.js
+++ spec/javascripts/tests/Email_spec.js
@@ -12,7 +12,7 @@ describe.only('Email', function() {
         _this.email = Email;
         _this.obj = new _this.email(_this.component);
 
-        _this.resultsTables = _this.$html.find('[data-dough-results-table]');
+        _this.$emailLink = _this.component.find('[data-dough-email-link]');
 
         done();
     }, done);
@@ -22,34 +22,35 @@ describe.only('Email', function() {
     this.$html.remove();
   });
 
-  describe('Creates content from values in fixture', function() {
-    it('Does nothing yet', function() {
+  describe('Creates email link href from results', function() {
+    beforeEach(function() {
       this.obj.init();
+    });
 
-      console.log(this.component);
+    it('Tests the email link is correct', function() {
+      var message = '';
 
-      var message =
-        '1. Your details: 31 years, male, £50,000 per year, part salary' +
-        '2. Your contributions: You: 1%, Your employer: 1%' +
-        '3. Results:' +
-        'Qualifying Earnings: £39,124' +
-        'Now:' +
-        'Your contribution: £32.60' +
-        '(includes tax relief of £6.52)' +
-        'Employer\'s contribution: £32.60' +
-        'Total contributions: £65.20' +
-        'April 2018 - March 2019:' +
-        'Your contribution: £97.81' +
-        '(includes tax relief of £19.56)' +
-        'Employer\'s contribution: £65.21' +
-        'Total contributions: £163.02' +
-        'April 2019 onwards:' +
-        'Your contribution: £163.02' +
-        '(includes tax relief of £32.60)' +
-        'Employer\'s contribution: £97.81' +
-        'Total contributions: £260.83';
+      message += '1. Your details: 31 years, male, £50,000 per year, part salary\n\n';
+      message += '2. Your contributions: You: 1%, Your employer: 1%\n\n';
+      message += '3. Results: Qualifying Earnings: £39,124\n\n';
+      message += 'Now\n';
+      message += 'Your contribution: £32.60 (includes tax relief of £6.52)\n';
+      message += 'Employer\'s contribution: £32.60\n';
+      message += 'Total contributions: £65.20\n\n';
+      message += 'April 2018 - March 2019:\n';
+      message += 'Your contribution: £97.81\n';
+      message += '(includes tax relief of £19.56)\n';
+      message += 'Employer\'s contribution: £65.21\n';
+      message += 'Total contributions: £163.02\n\n';
+      message += 'April 2019 onwards:\n';
+      message += 'Your contribution: £163.02\n';
+      message += '(includes tax relief of £32.60)\n';
+      message += 'Employer\'s contribution: £97.81\n';
+      message += 'Total contributions: £260.83';
 
-      expect(1).to.equal(1);
+      var href = 'mailto:?body=' + encodeURIComponent(message);
+
+      expect(this.$emailLink.prop('href')).to.equal(href);
     });
   });
 });
