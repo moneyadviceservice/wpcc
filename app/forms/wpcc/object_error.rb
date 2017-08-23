b64e00aca36c8a08953310f736290833553e1481
John Player <johnplayer1982@msn.com>
2017-08-23 13:05:35 +0100

    Adjusting dangling attributes

--- app/forms/wpcc/object_error.rb
+++ app/forms/wpcc/object_error.rb
@@ -1,8 +1,10 @@
 module Wpcc
   class ObjectError < Dough::Forms::ObjectError
+    AGE_I18N_KEY =
+      'activemodel.errors.models.wpcc/your_details_form.attributes.age'.freeze
     MESSAGES = [
-      I18n.t('activemodel.errors.models.wpcc/your_details_form.attributes.age.greater_than_or_equal_to'),
-      I18n.t('activemodel.errors.models.wpcc/your_details_form.attributes.age.less_than')
+      I18n.t("#{AGE_I18N_KEY}.greater_than_or_equal_to"),
+      I18n.t("#{AGE_I18N_KEY}.less_than")
     ].freeze
 
     def full_message
