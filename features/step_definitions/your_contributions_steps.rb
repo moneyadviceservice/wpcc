fb8c8b4bce745302bf2c2d38c57adb3fa1ef7cdc
Will Hall <willhall88@gmail.com>
2017-11-29 12:25:35 +0000

    Add feature near lower pension limit messages

--- features/step_definitions/your_contributions_steps.rb
+++ features/step_definitions/your_contributions_steps.rb
@@ -81,3 +81,11 @@ end
 Then(/^I should not see any salary "([^"]*)"$/) do |message|
   expect(your_contributions_page).to_not have_content(message)
 end
+
+Then(/^I should( not| NOT)? see the warning "([^"]*)"$/) do |should_not, message|
+  if should_not
+    expect(your_contributions_page).to_not have_content(message)
+  else
+    expect(your_contributions_page).to have_content(message)
+  end
+end
