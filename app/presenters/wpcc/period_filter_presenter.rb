856a9d9cae5245748843ff10e33efa79c6c4e8d3
Kate Wilkinson <kate.wilkinson@hotmail.com>
2018-02-20 10:00:31 +0000

    8908: Remove surplus messaging related to 2018 contribution increase

--- app/presenters/wpcc/period_filter_presenter.rb
+++ app/presenters/wpcc/period_filter_presenter.rb
@@ -1,17 +1,14 @@
 module Wpcc
   class PeriodFilterPresenter < Presenter
     def contribution_percents_explanation
-      @second_period = periods[1]
       @last_period = periods.last
 
       if user_inputs_greater_than_legal_minimums?
-        t('wpcc.results.large_contribution_percent_for_two_periods')
-      elsif user_inputs_between_2nd_and_3rd_period_legal_minimums?
-        t('wpcc.results.large_contribution_percent_for_middle_period')
+        t('wpcc.results.contribution_percentages_over_minimum')
       elsif user_input_for_employee_percent_greater_than_legal_minimums?
-        t('wpcc.results.employee_large_contribution_percent_for_two_periods')
+        t('wpcc.results.employee_contribution_percentage_over_minimum')
       elsif user_input_for_employer_percent_greater_than_legal_minimums?
-        t('wpcc.results.employer_large_contribution_percent_for_two_periods')
+        t('wpcc.results.employer_contribution_percentage_over_minimum')
       end
     end
 
@@ -20,13 +17,6 @@ module Wpcc
     def user_inputs_greater_than_legal_minimums?
       user_input_for_employee_percent_greater_than_legal_minimums? &&
         user_input_for_employer_percent_greater_than_legal_minimums?
-    end
-
-    def user_inputs_between_2nd_and_3rd_period_legal_minimums?
-      user_input_employee_percent >= @second_period.employee_percent &&
-        user_input_employee_percent < @last_period.employee_percent &&
-        user_input_employer_percent >= @second_period.employer_percent &&
-        user_input_employer_percent < @last_period.employer_percent
     end
 
     def user_input_for_employee_percent_greater_than_legal_minimums?
