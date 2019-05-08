c5c69f048857344402833a5af340cb7aaf695f87
Margo Urey <margo@margonline.co.uk>
2019-05-08 16:23:33 +0100

    tests for new miminum validation component

--- spec/javascripts/test-main.js
+++ spec/javascripts/test-main.js
@@ -1,16 +1,16 @@
-var allTestFiles = []
-var TEST_REGEXP = /(spec|test)\.js$/i
+var allTestFiles = [];
+var TEST_REGEXP = /(spec|test)\.js$/i;
 
 // Get a list of all the test files to include
-Object.keys(window.__karma__.files).forEach(function (file) {
+Object.keys(window.__karma__.files).forEach(function(file) {
   if (TEST_REGEXP.test(file)) {
     // Normalize paths to RequireJS module names.
     // If you require sub-dependencies of test files to be loaded as-is (requiring file extension)
     // then do not normalize the paths
-    var normalizedTestModule = file.replace(/^\/base\/|\.js$/g, '')
-    allTestFiles.push(normalizedTestModule)
+    var normalizedTestModule = file.replace(/^\/base\/|\.js$/g, '');
+    allTestFiles.push(normalizedTestModule);
   }
-})
+});
 
 require.config({
   // Karma serves files under /base, which is the basePath from your config file
@@ -27,16 +27,21 @@ require.config({
     jquery: 'vendor/assets/bower_components/jquery/dist/jquery',
 
     // Internal modules
-    DoughBaseComponent: 'vendor/assets/bower_components/dough/assets/js/components/DoughBaseComponent',
+    DoughBaseComponent:
+      'vendor/assets/bower_components/dough/assets/js/components/DoughBaseComponent',
 
     // Dough components
     utilities: 'vendor/assets/bower_components/dough/assets/js/lib/utilities',
 
     // WPCC components
-    ConditionalMessaging: 'app/assets/javascripts/wpcc/components/ConditionalMessaging',
+    ConditionalMessaging:
+      'app/assets/javascripts/wpcc/components/ConditionalMessaging',
     SalaryConditions: 'app/assets/javascripts/wpcc/components/SalaryConditions',
-    ContributionConditions: 'app/assets/javascripts/wpcc/components/ContributionConditions',
+    ContributionConditions:
+      'app/assets/javascripts/wpcc/components/ContributionConditions',
+    ContributionsMinimumValidation:
+      'app/assets/javascripts/wpcc/components/ContributionsMinimumValidation',
     UpdateResults: 'app/assets/javascripts/wpcc/components/UpdateResults',
     Email: 'app/assets/javascripts/wpcc/components/Email'
   }
-})
+});
