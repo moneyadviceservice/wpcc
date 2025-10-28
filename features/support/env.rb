36ec1eacdf907bf1301836aa02da3d64cb6abf48
Ben Lovell <benjamin.lovell@gmail.com>
2025-10-28 15:04:44 +0000

    Switch to chromedriver
    PhantomJS sucks.

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,9 +2,20 @@ ENV['RAILS_ENV'] ||= 'test'
 ENV['RAILS_ROOT'] ||= File.dirname(__FILE__) + '../../../spec/dummy'
 
 require 'cucumber/rails'
-require 'capybara/poltergeist'
+require 'selenium/webdriver'
 require 'rspec/rails'
 
 ActionController::Base.allow_rescue = false
-Capybara.javascript_driver = :poltergeist
+
+Capybara.register_driver :chrome_headless do |app|
+  options = Selenium::WebDriver::Chrome::Options.new(
+    args: %w(headless no-sandbox disable-gpu window-size=2500,2500)
+  )
+
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
+
+Capybara.javascript_driver = :chrome_headless
+Capybara.default_max_wait_time = 20
+
 Cucumber::Rails::Database.autorun_database_cleaner = false
